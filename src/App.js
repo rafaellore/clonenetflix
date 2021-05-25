558c1db2686ac9809d11e1b3e34d2592d057e7e9
rafaellore <birdneon3@gmail.com>
2021-05-25 18:21:34 -0300

    dia 1, projeto sem anotações

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+export default () => {
+
+  /*Quando a página for carregada, ela irá executar está função */
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect( () =>{
+    const loadAll = async () =>{
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen =originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      console.log(chosenInfo);
+      setFeaturedData(chosenInfo);
+    }
+    loadAll();
+  }, [])
+
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      } else{
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+
+    return () =>{
+      window.removeEventListener('scroll',scrollListener)
+    }
+  },[]);
+
+  return(
+    <div className="page">
+      
+      <Header black={blackHeader}/>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+      }
+
+      <section className="lists">
+        {movieList.map((item,key)=>(
+        <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> por Rafael Moreira<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site Themoviedb.com
+      </footer>
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading"></img>
+      </div>
+      }
+    </div>
+  );
+}
---
+++ src/Tmdb.js
@@ -0,0 +1,67 @@
+const API_KEY = 'f2d93d773333142b048db3523e54f38e';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+
+const basicFetch = async (endpoint) =>{
+    return (await fetch(`${API_BASE}${endpoint}`)).json();
+}
+
+export default {
+     getHomeList : async () =>{
+        return [
+            {
+                slug: 'originals',
+                title : "Originais do Netflix",
+                items : await basicFetch(`/discover/tv/?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title : "Recomendados para Você",
+                items : await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title : "Em Alta",
+                items : await basicFetch(`/movie/top_rated?&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title : "Ação",
+                items : await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title : "Comédia",
+                items : await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title : "Terror",
+                items : await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title : "Romance",
+                items : await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },            
+            {
+                slug: 'documentary',
+                title : "Documentários",
+                items : await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ]
+      },
+      getMovieInfo: async (movieId, type ) =>{
+        let info = {};
+        switch(type){
+            case 'movie':
+                info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+            break;
+            case 'tv':
+                info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+            break;
+
+        }
+        return info;
+      }
+    }
